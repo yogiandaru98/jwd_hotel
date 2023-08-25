2a7487825e2b4623ca5da84fc0e483594977b71d
YOGI ANDARU <83377619+yogiandaru98@users.noreply.github.com>
2023-08-26 01:48:31 +0700

    benerin produk

--- controller/func.php
+++ controller/func.php
@@ -1,9 +1,27 @@
 <?php 
+/**
+ * Establishes a connection to the MySQL database server using the mysqli extension.
+ *
+ * @param string $host The hostname or IP address of the MySQL server.
+ * @param string $username The MySQL user name.
+ * @param string $password The MySQL password.
+ * @param string $database The name of the MySQL database to use.
+ * @return mysqli|false Returns a mysqli object on success, or false on failure.
+ */
+$conection = mysqli_connect("localhost","root","","hotel_jwd");
 
-//db connection pdo
-function db(){
-    $db = new PDO('mysql:host=localhost;dbname=hotel;charset=utf8', 'root', '');
-    return $db;
+
+function getProdukWithGallery(){
+    global $conection;
+    $query = "SELECT class.*, rooms_gallery.* FROM class INNER JOIN rooms_gallery ON class.id = rooms_gallery.id_class";
+    $result = mysqli_query($conection, $query);
+    $data = [];
+    while($row = mysqli_fetch_assoc($result)){
+        $data[] = $row;
+    }
+    // return var_dump($data);
+    return $data;
 }
+
 
 ?>
--- pages/produk.php
+++ pages/produk.php
@@ -1,43 +1,49 @@
 <?php $activePage = 'produk'; ?>
 <?php include 'header.php' ?>
+<?php include '../controller/func.php' ?>
+<?php $produkData = getProdukWithGallery(); ?>
+<?php var_dump($produkData) ?>
 <div class="container mt-5">
-
     <div class="d-flex align-content-start d-flex justify-content-between flex-wrap">
-        <div class="card m-2" style="width: 300px; height:310px">
-            <div id="myCarousel" class="carousel slide" data-ride="carousel" style="width: 300px; height: 200px;">
-                <ol class="carousel-indicators">
-                    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-                    <li data-target="#myCarousel" data-slide-to="1"></li>
-                    <li data-target="#myCarousel" data-slide-to="2"></li>
-                </ol>
+        <?php $prevIdClass = null; ?>
+        <?php foreach ($produkData as $produk) : ?>
+            <?php if ($prevIdClass !== $produk['id_class']) : ?>
+                <?php $prevIdClass = $produk['id_class']; ?>
 
-                <div class="carousel-inner">
-                    <div class="carousel-item active">
-                        <img width="300" height="200" src="https://backend.parador-hotels.com/wp-content/uploads/2023/04/Deluxe-Room-Artinya-1024x512.jpg" alt="Gambar 1">
+                <div class="card m-2" style="width: 300px; height: 310px;">
+                    <div id="myCarousel-<?php echo $produk['id_class']; ?>" class="carousel slide" data-ride="carousel" style="width: 300px; height: 200px;">
+
+                        <div class="carousel-inner">
+                            <?php $firstImage = true; ?>
+                            <?php foreach ($produkData as $item) : ?>
+                                <?php if ($item['id_class'] == $produk['id_class']) : ?>
+                                    <div class="carousel-item <?php if ($firstImage && $item['type'] == 'image') echo 'active'; ?>">
+                                        <?php if ($item['type'] == 'image') : ?>
+                                            <img width="300" height="200" src="<?php echo $item['file']; ?>" alt="<?php echo $item['name']; ?>">
+                                            <?php $firstImage = false; ?>
+                                        <?php elseif ($item['type'] == 'video') : ?>
+                                            <iframe width="300" height="200" src="<?php echo $item['file']; ?>" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+                                        <?php endif; ?>
+                                    </div>
+                                <?php endif; ?>
+                            <?php endforeach; ?>
+                        </div>
+
+
+                        <a class="carousel-control-prev" href="#myCarousel-<?php echo $produk['id_class']; ?>" data-slide="prev">
+                            <span class="carousel-control-prev-icon"></span>
+                        </a>
+                        <a class="carousel-control-next" href="#myCarousel-<?php echo $produk['id_class']; ?>" data-slide="next">
+                            <span class="carousel-control-next-icon"></span>
+                        </a>
                     </div>
-                    <div class="carousel-item">
-                        <iframe width="300" height="200" src="https://www.youtube.com/embed/VmAn32gKhF0" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                    </div>
-                    <div class="carousel-item">
-                        <img width="300" height="200" src="https://www.kokoonhotelsvillas.com/surabaya/wp-content/uploads/sites/2/2020/01/standard-room.jpg" alt="Gambar 2">
+                    <div class="ml-2">
+                        <h1><?php echo ucfirst($produk['name']); ?></h1>
+                        <h3>Rp. <?php echo number_format($produk['price'], 0, ',', '.'); ?></h3>
                     </div>
                 </div>
-
-                <a class="carousel-control-prev" href="#myCarousel" data-slide="prev">
-                    <span class="carousel-control-prev-icon"></span>
-                </a>
-                <a class="carousel-control-next" href="#myCarousel" data-slide="next">
-                    <span class="carousel-control-next-icon"></span>
-                </a>
-            </div>
-            <div class="ml-2">
-
-                <h1>Standar</h1>
-                <h3>Rp. 500.000</h3>
-            </div>
-        </div> 
+            <?php endif; ?>
+        <?php endforeach; ?>
     </div>
 </div>
-
-
 <?php include 'footer.php' ?>
